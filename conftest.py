a69af37b69601b4d8bd8a6c37c10545fc2fd5694
akhm <cool.kagazezhev@gmail.com>
2026-01-24 18:29:36 +0300

    123

--- conftest.py
+++ conftest.py
@@ -7,6 +7,8 @@ def driver(request):
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
